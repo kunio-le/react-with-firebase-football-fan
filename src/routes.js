3e9da7b9ff51bd07c3e614b91e3c07b30d3e0439
liguoxiong <bunyhung@gmail.com>
2019-04-13 07:46:28 +0700

    "Add resource"

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
 import React, { Fragment } from 'react';
 
 import { Route } from 'react-router-dom';
+
+import Layout from './hoc/Layout';
 import HomePage from './pages/HomePage';
 import TheTeam from './pages/TheTeam';
 import Matches from './pages/Matches';
 
 const Routes = () => (
     <Fragment>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/the-team" component={TheTeam} />
-        <Route exact path="/matches" component={Matches} />
+        <Layout>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/the-team" component={TheTeam} />
+            <Route exact path="/matches" component={Matches} />
+        </Layout>
     </Fragment>
 );
 
